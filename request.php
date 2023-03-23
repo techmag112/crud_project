952a038497ddd1aa63ddc471ad08860932ec840b
Oleg Khayrullin <oleg.khayrullin@gmail.com>
2023-03-23 18:31:26 +0300

    ref request

--- request.php
+++ request.php
@@ -6,41 +6,32 @@
     $email = $_POST['email'] ?? null;
     $tel = $_POST['tel'] ?? null;
     $get_id = $_GET['id'] ?? null;
-
-// Create
+    
     if (isset($_POST['create'])) {
-        $query = "INSERT INTO customers (name, email, tel) VALUES (?, ?, ?)";
-        $result = $pdo->prepare($query);
-        $result->execute(array($name, $email, $tel));
-        if ($result) {
-            header('Location: ' . $_SERVER['HTTP_REFERER']);
-        }
-    }
+        requestRun($pdo, "INSERT INTO customers (name, email, tel) VALUES (?, ?, ?)", array($name, $email, $tel));
+        unset($_POST['create']);
+    };
 
-// Read
+    if (isset($_POST['edit'])) {
+        requestRun($pdo, "UPDATE customers SET name=?, email=?, tel=? WHERE id=?", array($name, $email, $tel, $get_id));
+        unset($_POST['edit']);
+    };
+
+    if (isset($_POST['delete'])) {
+        // Запись не удаляем, а помечаем для удаления, чтобы можно было восстановить
+        requestRun($pdo, "UPDATE customers SET flag = 1 WHERE id=?", array($get_id));
+        unset($_POST['delete']);
+    };
+
     $query = $pdo->prepare("SELECT * FROM customers WHERE flag = 0");
     $query->execute();
     $result = $query->fetchAll(PDO::FETCH_OBJ);
 
-// Update
-if (isset($_POST['edit'])) {
-    $query = "UPDATE customers SET name=?, email=?, tel=? WHERE id=?";
-    $result = $pdo->prepare($query);
-    $result->execute(array($name, $email, $tel, $get_id));
-    if ($result) {
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
+    function requestRun($db, $sqlString, $arr) {
+        $result = $db->prepare($sqlString);
+        $result->execute($arr);
+        // Обновить страницу
+        if ($result) {
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+        }
     }
-}
-
-// Delete
-if (isset($_POST['delete'])) {
-    // $query = "DELETE FROM customers WHERE id=?";
-    $query = "UPDATE customers SET flag = 1 WHERE id=?";
-    $result = $pdo->prepare($query);
-    $result->execute(array($get_id));
-    if ($result) {
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
-    }
-}
-
-   
